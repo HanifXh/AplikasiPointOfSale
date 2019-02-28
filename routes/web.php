307070d3b52be5769b18265d9182f731f9bab2be
HanifXh <hanifhairulloh19@gmail.com>
2019-02-28 15:22:34 +0700

    selesai buat tampilan depan login masukin lte dan font awesome

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('tanggal', function () {
+    
+    echo tanggal_indonesia(date('y-m-d')); 
+    
+});
